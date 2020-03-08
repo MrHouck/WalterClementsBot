d2926b51846f92cd2cc76d7dd116689e73235b01
MrHouck <nhouck51@gmail.com>
2020-03-08 11:47:38 -0400

    Update help.py
    v1.8.1

--- cogs/help.py
+++ cogs/help.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from datetime import datetime
 
 class Help(commands.Cog):
     def __init__(self, client):
@@ -7,17 +8,17 @@ class Help(commands.Cog):
 
     @commands.command(pass_context=True)
     @commands.guild_only()
-    async def help(self, ctx, command=None):
-        if command == None:
-            await ctx.trigger_typing()
-            embed=discord.Embed(title="Help", color=0x4966cb)
-            embed.add_field(name="🥳 **Fun:**", value="``8ball, achievement, fbi, norris, penis, randomname, retard, sentence, uwu, walter``", inline=False)
-            embed.add_field(name="🛠️ **Moderation:**", value="``autorole[enable, disable, setrole], ban, clear, createrole, deleterole, logs[enable, disable, setchannel], mute, unban, unmute, role, softban``", inline=False)
-            embed.add_field(name="📈 **Economy:**", value="``balance, buy, daily, fish, register, shop, stats, stocks[buy, sell, view]``")
-            embed.add_field(name="💎 **Miscellaneous:**", value="``changelog, color, help, invite, serverinfo, usage, userinfo, suggestion``", inline=False)
-            embed.set_footer(text="WalterClementsBot - by MrHouck#2775 | v1.7.4")
-            await ctx.send(embed=embed)
-
+    async def help(self, ctx):
+        await ctx.trigger_typing()
+        embed=discord.Embed(title="Help", color=0x4966cb)
+        embed.add_field(name="🥳 **Fun:**", value="``8ball, achievement, bird, cat, catfact, chatbot, dog, dogfact, fbi, hug, koalafact, norris, panda, pandafact, penis, randomname, retard, sentence, space, uwu, walter``", inline=False)
+        embed.add_field(name="🛠️ **Moderation:**", value="``autorole[enable, disable, setrole], ban, clear, createrole, deleterole, lockdown[on, off], logs[enable, disable, setchannel], mute, unban, unmute, role, softban``", inline=False)
+        embed.add_field(name="📈 **Economy/Levels:**", value="``balance, buy, daily, fish, register, shop, stats``")
+        embed.add_field(name="💎 **Miscellaneous:**", value="``changelog, complementary, google, hex, help, invite, serverinfo, source, suggestion, userinfo, urban, wikipedia``", inline=False)
+        embed.set_footer(text="WalterClementsBot - by MrHouck#2775 | v1.8.1")
+        await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(Help(client))
+    now = datetime.now()
+    print(f"{now} | Loaded help module.")
