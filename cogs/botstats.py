032b33a4de84110423cfb26734b4284796202556
MrHouck <nhouck51@gmail.com>
2020-03-09 21:38:41 -0400

    Create botstats.py
    v1.8.4

---
+++ cogs/botstats.py
@@ -0,0 +1,52 @@
+import discord, datetime, time
+from discord.ext import commands
+
+start_time = time.time()
+
+class BotStats(commands.Cog):
+    def __init__(self, client):
+        self.bot = client
+
+    @commands.command()
+    async def uptime(self, ctx):
+        current_time = time.time()
+        difference = current_time - start_time
+        text = str(datetime.timedelta(seconds=difference))
+        embed = discord.Embed(color=ctx.author.color)
+        embed.add_field(name="Uptime", value=text)
+        try:
+            await ctx.send(embed=embed)
+        except discord.HTTPException:
+            await ctx.send("Current uptime: " + text)
+
+    @commands.command()
+    async def botstats(self, ctx):
+        #UPTIME
+        current_time = time.time()
+        difference = current_time - start_time
+        text = str(datetime.timedelta(seconds=difference))
+        #GUILDS
+        guildNum = len(self.bot.guilds)
+        #get users
+        totalMembers = 0
+        for guild in self.bot.guilds:
+            totalMembers += guild.member_count
+
+        ping = self.bot.latency
+        if ping < 100:
+            color = discord.Color.green()
+        elif ping > 100 and ping < 200:
+            color = discord.Color.from_rgb(0, 34, 255)
+        else:
+            color = discord.Color.red()
+        embed = discord.Embed(title="Walter Clements Bot Statistics", color=color)
+        embed.add_field(name="Uptime", value='{}'.format(text))
+        embed.add_field(name="Guild Count", value='{}'.format(guildNum), inline=False)
+        embed.add_field(name="Total Members", value='{}'.format(totalMembers), inline=False)
+        embed.add_field(name="Client Latency", value='{}'.format(ping), inline=False)
+        await ctx.send(embed=embed)
+
+def setup(client):
+    client.add_cog(BotStats(client))
+    now = datetime.datetime.now()
+    print(f"{now} | Loaded botstats module.")
